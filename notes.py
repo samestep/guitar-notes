dd6fc6a0ca4679cf46a5c219873225ce0e16f8b2
Sam Estep <sam@samestep.com>
2019-05-21 16:01:27 -0400

    Add some random stuff I did last fall
    Also update the LICENSE: see
    https://softwareengineering.stackexchange.com/a/147120/301466

--- notes.py
+++ notes.py
@@ -51,10 +51,115 @@ def fill_measure(notes):
 strings = [('e', 4), ('b', 3), ('g', 3), ('d', 3), ('a', 2), ('e', 2)]
 board = fretboard(strings)
 
-print('\\version "2.18.2" { \\clef "treble_8" \\time 3/4')
-for measure in range(100):
-    note = random.choice(list(board.keys()))
-    count = board[note]
-    spell = random.choice(list(spellings(note)))
-    print(fill_measure(count * [scientific_to_lilypond(spell)]))
-print('}')
+def positions(chord):
+    chord = list(chord)
+    answers = []
+    for permutation in itertools.permutations(range(len(strings)), len(chord)):
+        frets = {}
+        for note_index, string_index in enumerate(permutation):
+            note = integer_notation(chord[note_index])
+            string = integer_notation(strings[string_index])
+            for fret in range(21):
+                if note == move_up(string, fret):
+                    frets[string_index] = fret
+        if len(frets) == len(chord):
+            answers.append(frets)
+    return answers
+
+def ease(position, capo):
+    frets = set(position.values()) - {capo}
+    candidate = True
+    for fret in frets:
+        if fret < capo:
+            candidate = False
+    if candidate:
+        return max(frets) - min(frets) if len(frets) > 0 else 0
+
+def easiest(choices, capo):
+    best = None
+    for position in choices:
+        position_ease = ease(position, capo)
+        if best == None or (position_ease != None and position_ease < best):
+            best = position_ease
+    return best
+
+def display_string(index, position, capo):
+    if index in position:
+        return str(position[index]).rjust(2)
+    else:
+        return '  '
+
+def display_position(position, capo):
+    strings = [display_string(index, position, capo) for index in range(6)]
+    print(' '.join(strings))
+
+chordses = [
+    [
+        {('f', 3), ('f', 4), ('a', 4), ('c', 5)},
+        {('e', 3), ('g', 4), ('c', 5)},
+        {('d', 3), ('f', 4), ('a', 4), ('c', 5)},
+        {('c', 3), ('f', 4), ('a', 4), ('c', 5)},
+        {('bes', 2), ('f', 4), ('g', 4), ('d', 5)},
+        {('c', 3), ('e', 4), ('g', 4), ('c', 5)},
+        {('f', 3), ('f', 4), ('a', 4), ('c', 5)},
+        {('c', 3), ('e', 4), ('g', 4), ('c', 5)}
+    ],
+    [
+        {('f', 3), ('c', 4), ('f', 4), ('a', 4)},
+        {('e', 3), ('c', 4), ('g', 4)},
+        {('d', 3), ('c', 4), ('f', 4), ('a', 4)},
+        {('c', 3), ('c', 4), ('f', 4), ('a', 4)},
+        {('bes', 2), ('d', 4), ('f', 4), ('g', 4)},
+        {('c', 3), ('c', 4), ('e', 4), ('g', 4)},
+        {('f', 3), ('c', 4), ('f', 4), ('a', 4)},
+        {('c', 3), ('c', 4), ('e', 4), ('g', 4)}
+    ],
+    [
+        {('f', 3), ('a', 3), ('c', 4), ('f', 4)},
+        {('e', 3), ('g', 3), ('c', 4), ('g', 4)},
+        {('d', 3), ('a', 3), ('c', 4), ('f', 4)},
+        {('c', 3), ('a', 3), ('c', 4), ('f', 4)},
+        {('bes', 2), ('g', 3), ('d', 4), ('f', 4), ('g', 4)},
+        {('c', 3), ('g', 3), ('c', 4), ('e', 4), ('g', 4)},
+        {('f', 3), ('a', 3), ('c', 4), ('f', 4)},
+        {('c', 3), ('g', 3), ('c', 4), ('e', 4)}
+    ]
+]
+
+# for chords in chordses:
+#     for capo in range(21):
+#         eases = [easiest(positions(chord), capo) for chord in chords]
+#         if None in set(eases):
+#             break
+#         print(capo, eases)
+#     print()
+
+capos = [1, 3, 3]
+
+for i, chords in enumerate(chordses):
+    capo = capos[i]
+    print(' e  B  G  D  A  E')
+    for chord in chords:
+        best_closed = None
+        best_ease = None
+        best_pos = None
+        best_position = None
+        for position in positions(chord):
+            position_closed = len([fret for fret in position.values() if fret != capo])
+            position_ease = ease(position, capo)
+            position_pos = None
+            if position_ease != None and position_ease < 4 and (best_closed == None or position_closed < best_closed):
+                best_ease = position_ease
+                best_closed = position_closed
+                best_pos = position_pos
+                best_position = position
+        display_position(best_position, capo)
+    print()
+
+# print('\\version "2.18.2" { \\clef "treble_8" \\time 3/4')
+# for measure in range(100):
+#     note = random.choice(list(board.keys()))
+#     count = board[note]
+#     spell = random.choice(list(spellings(note)))
+#     print(fill_measure(count * [scientific_to_lilypond(spell)]))
+# print('}')
